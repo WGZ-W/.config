99ddfb31b09c4f410d9368c5a2de20693a6f2b53
wgz <371960029@qq.com>
2024-10-14 20:24:37 +0800

    set the font size for lab computer

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,10 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=20" };
-static const char dmenufont[]       = "monospace:size=20";
+// static const char *fonts[]          = { "monospace:size=24" }; // 24 inch
+// static const char dmenufont[]       = "monospace:size=24";     // 24 inch
+static const char *fonts[]          = { "monospace:size=32" }; // 27 inch lab's computer
+static const char dmenufont[]       = "monospace:size=32";     // 27 inch lab's computer
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -26,7 +28,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,7 +55,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod1Mask // Alt key
+// #define MODKEY Mod4Mask    // Win key
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
--- slstatus/config.h
+++ slstatus/config.h
@@ -66,4 +66,6 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ datetime, "%s",           "%F %T" },
+//  { battery_perc, "",          "" },
+//	{ vol_perc, "%s",          "" },
 };
